677d67dc6e355d6cbf7c8478ef2ac1c144dce00d
Austin <austin.m.reed1@outlook.com>
2025-08-24 19:25:12 -0500

    Initial commit for Personal Info Printer

---
+++ PersonalInfoPrinter.java
@@ -0,0 +1,15 @@
+public class PersonalInfoPrinter {
+    public static void main(String[] args) {
+        String firstName = "Austin";
+        String lastName = "Reed";
+        String streetAddress = "10617 CR 494";
+        String city = "Tyler";
+        String zipCode = "75707";
+
+        System.out.println(firstName);
+        System.out.println(lastName);
+        System.out.println(streetAddress);
+        System.out.println(city);
+        System.out.println(zipCode);
+    }
+}
